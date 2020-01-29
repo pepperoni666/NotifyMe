7b557d303107ee802883b56451e76d33562c3599
Olga Perets <peretser08@gmail.com>
2020-01-30 00:01:19 +0100

    start of building notifyme test ui

--- notifyMeAndroidApp/app/src/main/java/com/pepperoni/android/notifyme/MainActivity.kt
+++ notifyMeAndroidApp/app/src/main/java/com/pepperoni/android/notifyme/MainActivity.kt
@@ -2,14 +2,10 @@ package com.pepperoni.android.notifyme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.text.SpannableStringBuilder
-import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -44,31 +40,26 @@ class MainActivity : AppCompatActivity() {
                         .addOnCompleteListener { idTask ->
                             if (idTask.isSuccessful) {
                                 idTask.result?.token?.let {
-                                    app.db.collection("receivers").document(it).set({
-                                        val test = "test"
-                                    })
+                                    app.db.collection("receivers").document(it).set(
+                                        hashMapOf(
+                                            "test" to 222
+                                        )).addOnSuccessListener {
+                                        Toast.makeText(baseContext, "Data saved successfully.",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
                                 }
                             }
 
-                            // Get new Instance ID token
                             val token = idTask.result?.token
 
-                            // Log and toast
                             main_text.text = SpannableStringBuilder(token)
                         }
-                    // Sign in success, update UI with the signed-in user's information
-                    //Log.d(TAG, "signInAnonymously:success")
                     val user = app.auth.currentUser
-                    //updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "signInAnonymously:failure", authTask.exception)
+
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
                 }
-
-                // ...
             }
     }
 }
---
+++ notifyMeAndroidApp/app/src/main/java/com/pepperoni/android/notifyme/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.pepperoni.android.notifyme.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+}
